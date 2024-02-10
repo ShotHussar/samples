ad088ec67c2f86807fdf528b8e60f2dbff23beae
ShotHussar <huntlef9802@gmail.com>
2024-02-10 16:48:14 -0600

    Woah that is a lot of commits

---
+++ core/getting-started/unit-testing-using-dotnet-test/PrimeService.Tests/PrimeService_IsPrimeShould.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+using Prime.Services;
+
+namespace Prime.UnitTests.Services
+{
+    public class PrimeService_IsPrimeShould
+    {
+        private readonly PrimeService _primeService;
+
+        public PrimeService_IsPrimeShould()
+        {
+            _primeService = new PrimeService();
+        }
+
+        [Theory]
+        [InlineData(4)]
+        [InlineData(8)]
+        
+        public void IsPrime_Values_Divisible_By_2(int value)
+        {
+            var result = _primeService.IsPrime(value);
+
+            Assert.False(result, $"{value} should not be prime");
+        }
+    }
+}
---
+++ core/getting-started/unit-testing-using-dotnet-test/PrimeService/PrimeService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Prime.Services
+{
+    public class PrimeService
+    {
+        public bool IsPrime(int candidate)
+        {
+            int a = 0;
+            for (int i = 1; i <= candidate; i++)
+            {
+                if (candidate % i == 0)
+                {
+                    a++;
+                }
+            }
+            if (a == 2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+            throw new NotImplementedException("Not fully implemented.");
+        }
+    }
+}
